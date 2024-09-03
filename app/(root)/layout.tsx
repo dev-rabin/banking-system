3c4185539e2a5fee1986faf7cba01a0da0b53167
dev-rabin <robinmandhotia@gmail.com>
2024-09-03 22:07:28 +0530

    sidebar and sidebar links

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName : "Robin" , lastName : "Mandhotia"}
   return (
-   <main>
-     SIDEBAR 
-     {children}
-   </main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user= {loggedIn} />
+      {children}
+    </main>
   );
 }
--- components/ui/DoughnutChart.tsx
+++ components/ui/DoughnutChart.tsx
@@ -9,7 +9,7 @@ const data = {
     datasets : [
         {
             label : "Banks",
-            data : ["6350" , "9000", "12000"],
+            data : ["6350" , "3000", "2000"],
             backgroundColor : ['#0757b6', '#2265D8', '#2f91fa']
         }
     ],
@@ -17,7 +17,16 @@ const data = {
 }
 
 function DoughnutChart({accounts} : DoughnutChartProps) {
-  return <Doughnut data={data}/>
+  return <Doughnut data={data}
+  options={{
+    cutout : "60%",
+    plugins : {
+        legend : {
+            display : false
+        }
+    }
+  }}
+  />
 }
 
 export default DoughnutChart
